b10313ae44f25087958c205a41a33975b53ea6aa
Errol Sidelsky <>
2018-09-17 17:22:26 +0100

    FAQs Cutsom Post Type created

--- public/wp-content/themes/project-theme/functions.php
+++ public/wp-content/themes/project-theme/functions.php
@@ -29,8 +29,42 @@ add_theme_support( 'post-thumbnails' );
     add_action('init', 'createPostTypes');
     function createPostTypes()
     {
-        //CustomPostType::createPostType('Event', 'Events');
+        //CustomPostType::createPostType('FAQ', 'FAQs');
+
+        $instance = new CustomPostType();
+        $instance->createPostType('FAQ', 'FAQs');
+
     }
+
+    //Static functions, by definition, cannot and do not depend on any instance properties of the class. That is, they do not require an instance of the class to execute (and so can be executed. In some sense, this means that the function doesn't (and will never need to) depend on members or methods (public or private) of the class.
+
+    // class Example {
+
+    //     // property declaration
+    //     public $value = "The text in the property";
+    
+    //     // method declaration
+    //     public function displayValue() {
+    //         echo $this->value;
+    //     }
+    
+    //     static function displayText() {
+    //         echo "The text from the static function";
+    //     }
+    // }
+    
+    
+    // $instance = new Example();
+    // $instance->displayValue();
+    
+    // $instance->displayText();
+    
+    // // Example::displayValue();         // Direct call to a non static function not allowed
+    
+    // Example::displayText();
+
+
+
 
     //Taxonomies
     add_action('init', 'createTaxonomies');
