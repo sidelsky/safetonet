7a2d6d41999e18c97fe7ec133c80b59925b62d34
Errol Sidelsky <>
2018-09-19 15:26:07 +0100

    Working on downloads

--- public/wp-content/themes/project-theme/Theme/View/Components/c-downloads.php
+++ public/wp-content/themes/project-theme/Theme/View/Components/c-downloads.php
@@ -1,18 +1,25 @@
 <section class="u-section u-bg-color--bw-white">
-    <div class="u-l-container--center u-l-container--shallow" data-in-viewport>
-        <div class="u-l-container u-l-container--row u-l-horizontal-padding u-l-vertical-padding">
+    <div class="u-l-container--center u-l-container--small" data-in-viewport>
+        <div class="u-l-container u-l-container--row u-l-horizontal-padding u-l-vertical-padding--small">
             <?php foreach($data['downloads'] as $download) { ?>
-                <?= $download['deviceName']; ?>
-                <img src="<?= $download['typeOfDevice']; ?>">
-                <img src="<?= $download['deviceFeatureImage']; ?>">
-                <img src="<?= $download['appStoreLogo']; ?>">
-
-                <ul>
-                    <?php foreach($download['downloadDetails'] as $requirement) { ?>
-                        <li class="c-plan-card__detail"><?= $requirement['requirement'] ?></li>
-                    <?php } ?>  
-                </ul>
-
+                <div class="c-download-card">
+                    <?= $download['deviceName']; ?>
+                    <a href="/downloads">
+                        <div class="c-download-card__device">
+                            <img class="c-download-card__device-type" src="<?= $download['typeOfDevice']; ?>">
+                            <!-- <img src="<?= $download['deviceFeatureImage']; ?>"> -->
+                        </div>
+                        <img class="c-download-card__store-logo" src="<?= $download['appStoreLogo']; ?>">
+                    </a>
+                    <div class="c-download-card__details">
+                        <h4 class="c-site-headings--h4">Minimum Requirements</h4>
+                        <ul class="c-download-card__list">
+                            <?php foreach($download['downloadDetails'] as $requirement) { ?>
+                                <li><?= $requirement['requirement'] ?></li>
+                            <?php } ?>  
+                        </ul>
+                    </div>
+                </div>
             <?php } ?>
         </div>
     </div>
