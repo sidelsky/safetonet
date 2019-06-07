1f2ab010224328a31647568c6c993e3b3268aaf1
Errol Sidelsky <errol@squie.com>
2019-06-07 11:31:42 +0100

    Last

--- src/Assets/js/components/chat-widget.js
+++ src/Assets/js/components/chat-widget.js
@@ -1,92 +1,80 @@
-//Localhost contact page ID's
+//Localhost contact page ID's 
 let contactPageIDLocal = {
   contact_EN: '262',
   contact_ES: '719',
   contact_DE: '721'
 }
 
-//Staging and Live contact page ID's
+//Staging and Live contact page ID's 
 let contactPageIDLive = {
   contact_EN: '262',
   contact_ES: '694',
   contact_DE: '1045'
 }
 
-//Local
+//Local 
 let contact_EN_Local = document.querySelector(`.page-id-${contactPageIDLocal.contact_EN}`);
 let contact_ES_Local = document.querySelector(`.page-id-${contactPageIDLocal.contact_ES}`);
 let contact_DE_Local = document.querySelector(`.page-id-${contactPageIDLocal.contact_DE}`);
 
-//Live
+//Live 
 let contact_EN_Live = document.querySelector(`.page-id-${contactPageIDLive.contact_EN}`);
 let contact_ES_Live = document.querySelector(`.page-id-${contactPageIDLive.contact_ES}`);
 let contact_DE_Live = document.querySelector(`.page-id-${contactPageIDLive.contact_DE}`);
 
-/**
- * ES Form
+/** 
+ * ES Form 
  */
 if (contact_ES_Local || contact_ES_Live) {
 
   var preChatTemplate = {
-    //Form header color and Submit button color.
+    //Form header color and Submit button color. 
     mainbgColor: '#0aa4db',
-    //Form Header Text and Submit button text color.
+    //Form Header Text and Submit button text color. 
     maintxColor: '#fff',
-    //Chat Form Title
-    heading: 'SafeToNet Chat Directo',
-    //Chat form Welcome Message
-    textBanner: 'Hola, ¡bienvenido a SafeToNet! Para empezar a chatear con nosotros necesitamos algo más de información. Una vez la tengamos, ¡podremos ponerte en contacto con el siguiente agente disponible!',
-    //Submit Button Label.
+    //Chat Form Title 
+    heading: 'Habla Con Nosotros',
+    //Chat form Welcome Message 
+    textBanner: 'Hola, ¡bienvenido a SafeToNet! Para empezar a chatear con nosotros necesitamos algo más de información. Una vez la tengamos, ¡podremos ponerte en contacto con un miembro de nuestro equipo!',
+    //Submit Button Label. 
     SubmitLabel: 'Empezar Chat',
-    //Fields List - Maximum is 5
-    //All the values are mandatory and the script will not work if not available.
+    //Fields List - Maximum is 5 
+    //All the values are mandatory and the script will not work if not available. 
     fields: {
       field1: {
-        //Type can be either text or title
-        type: "title",
-        //Label for Field Title, can be in any language
-        label: "Tratamiento",
-        //Field ID for Title
-        fieldId: "title",
-        //Required "yes" or "no"
-        required: "yes",
-        //Error text to be displayed
-        error: "Por favor, introduce un tratamiento válido"
-      },
-      field2: {
-        //Type for Name - Do not Change
+        //Type for Name - Do not Change 
         type: "name",
-        //Label for Field Name, can be in any language
-        label: "Nombre",
-        //Default - Field ID for Name - Do Not Change
+        //Label for Field Name, can be in any language 
+        label: "Nombre Completo",
+        //Default - Field ID for Name - Do Not Change 
         fieldId: "name",
-        //Required "yes" or "no"
+        //Required "yes" or "no" 
         required: "yes",
-        //Error text to be displayed
+        //Error text to be displayed 
         error: "Por favor, introduce un nombre válido"
       },
-      field3: {
-        //Type for Email - Do Not Change
+      field2: {
+        //Type for Email - Do Not Change 
         type: "email",
-        //Label for Field Email, can be in any language
+        //Label for Field Email, can be in any language 
         label: "Correo electrónico",
-        //Default - Field ID for Email - Do Not Change
+        //Default - Field ID for Email - Do Not Change 
         fieldId: "email",
-        //Required "yes" or "no"
+        //Required "yes" or "no" 
         required: "yes",
-        //Error text to be displayed
+        //Error text to be displayed 
         error: "Por favor, introduce un correo válido"
       },
-      field4: {
-        //Type for Phone - Do Not Change
+      field3: {
+        //Type for Phone - Do Not Change 
         type: "phone",
-        //Label for Field Phone, can be in any language
-        label: "Teléfono",
-        //Default - Field ID for Phone - Do Not Change
+        //Label for Field Phone, can be in any language 
+        label: "Número de Teléfono",
+        //Default - Field ID for Phone - Do Not Change 
         fieldId: "phone",
-        //Required "yes" or "no"
+        //Required "yes" or "no" 
         required: "yes",
-        //Error text to be displayed
+        //Error text to be displayed 
         error: "Por favor, introduce un número de teléfono válido"
       }
     }
@@ -96,14 +84,14 @@ if (contact_ES_Local || contact_ES_Live) {
     host: "https://wchat.freshchat.com",
     config: {
       cssNames: {
-        //The below element is mandatory. Please add any custom class or leave the default.
+        //The below element is mandatory. Please add any custom class or leave the default. 
         widget: 'custom_fc_frame',
-        //The below element is mandatory. Please add any custom class or leave the default.
+        //The below element is mandatory. Please add any custom class or leave the default. 
         expanded: 'custom_fc_expanded'
       }
     },
     onInit: function () {
-      //console.log('widget init');
+      //console.log('widget init'); 
       fcPreChatform.fcWidgetInit(preChatTemplate);
     }
   };
@@ -112,72 +100,60 @@ if (contact_ES_Local || contact_ES_Live) {
 
 
 
-/**
- * DE Form
+/** 
+ * DE Form 
  */
 if (contact_DE_Local || contact_DE_Live) {
 
   var preChatTemplate = {
-    //Form header color and Submit button color.
-    mainbgColor: '#0aa4db',
-    //Form Header Text and Submit button text color.
-    maintxColor: '#fff',
-    //Chat Form Title
-    heading: 'SafeToNet Live Chat',
-    //Chat form Welcome Message
-    textBanner: 'Hallo, willkommen bei SafeToNet! Um mit uns zu chatten, brauchen wir ein paar Informationen von Ihnen. Wir werden Sie dann sofort mit dem nächsten verfügbaren Teammitglied verbinden!',
-    //Submit Button Label.
-    SubmitLabel: 'Chat starten',
-    //Fields List - Maximum is 5
-    //All the values are mandatory and the script will not work if not available.
+    //Form header color and Submit button color. 
+    mainbgColor: "#0aa4db",
+    //Form Header Text and Submit button text color. 
+    maintxColor: "#fff",
+    //Chat Form Title 
+    heading: "SafeToNet Live Chat",
+    //Chat form Welcome Message 
+    textBanner: "Hallo, willkommen bei SafeToNet! Um mit uns zu chatten, brauchen wir ein paar Informationen von Ihnen. Wir werden Sie dann sofort mit dem nächsten verfügbaren Teammitglied verbinden!",
+    //Submit Button Label. 
+    SubmitLabel: "Start Chat",
+    //Fields List - Maximum is 5 
+    //All the values are mandatory and the script will not work if not available. 
     fields: {
       field1: {
-        //Type can be either text or title
-        type: "title",
-        //Label for Field Title, can be in any language
-        label: "Anrede",
-        //Field ID for Title
-        fieldId: "title",
-        //Required "yes" or "no"
+        //Type for Name - Do not Change 
+        type: "name",
+        //Label for Field Name, can be in any language 
+        label: "Vollständiger Name",
+        //Default - Field ID for Name - Do Not Change 
+        fieldId: "name",
+        //Required "yes" or "no" 
         required: "yes",
-        //Error text to be displayed
-        error: "Bittegeben Sie eine gültige Anrede ein"
+        //Error text to be displayed 
+        error: "Bitte geben Sie einen gültigen Namen ein"
       },
       field2: {
-        //Type for Name - Do not Change
-        type: "name",
-        //Label for Field Name, can be in any language
-        label: "Name",
-        //Default - Field ID for Name - Do Not Change
-        fieldId: "name",
-        //Required "yes" or "no"
+        //Type for Email - Do Not Change 
+        type: "email",
+        //Label for Field Email, can be in any language 
+        label: "E-mail Adresse",
+        //Default - Field ID for Email - Do Not Change 
+        fieldId: "email",
+        //Required "yes" or "no" 
         required: "yes",
-        //Error text to be displayed
-        error: "Bittegeben Sie einen gültigen Namen ein"
+        //Error text to be displayed 
+        error: "Bitte geben Sie eine gültige E-mail Adresse ein"
       },
       field3: {
-        //Type for Email - Do Not Change
-        type: "email",
-        //Label for Field Email, can be in any language
-        label: "E-mail Adresse",
-        //Default - Field ID for Email - Do Not Change
-        fieldId: "email",
-        //Required "yes" or "no"
-        required: "yes",
-        //Error text to be displayed
-        error: "Bittegeben Sie eine gültige E-mail Adresse ein"
-      },
-      field4: {
-        //Type for Phone - Do Not Change
+        //Type for Phone - Do Not Change 
         type: "phone",
-        //Label for Field Phone, can be in any language
+        //Label for Field Phone, can be in any language 
         label: "Telefonnummer",
-        //Default - Field ID for Phone - Do Not Change
+        //Default - Field ID for Phone - Do Not Change 
         fieldId: "phone",
-        //Required "yes" or "no"
+        //Required "yes" or "no" 
         required: "yes",
-        //Error text to be displayed
-        error: "Bittegeben Sie eine gültige Telefonnummer ein"
+        //Error text to be displayed 
+        error: "Bitte geben Sie eine gültige Telefonnummer ein"
       }
     }
   };
@@ -186,14 +162,14 @@ if (contact_DE_Local || contact_DE_Live) {
     host: "https://wchat.freshchat.com",
     config: {
       cssNames: {
-        //The below element is mandatory. Please add any custom class or leave the default.
+        //The below element is mandatory. Please add any custom class or leave the default. 
         widget: 'custom_fc_frame',
-        //The below element is mandatory. Please add any custom class or leave the default.
+        //The below element is mandatory. Please add any custom class or leave the default. 
         expanded: 'custom_fc_expanded'
       }
     },
     onInit: function () {
-      //console.log('widget init');
+      //console.log('widget init'); 
       fcPreChatform.fcWidgetInit(preChatTemplate);
     }
   };
@@ -201,71 +177,59 @@ if (contact_DE_Local || contact_DE_Live) {
 }
 
 
-/**
- * EN Form
+/** 
+ * EN Form 
  */
 if (contact_EN_Local || contact_EN_Live) {
 
   var preChatTemplate = {
-    //Form header color and Submit button color.
+    //Form header color and Submit button color. 
     mainbgColor: '#0aa4db',
-    //Form Header Text and Submit button text color.
+    //Form Header Text and Submit button text color. 
     maintxColor: '#fff',
-    //Chat Form Title
+    //Chat Form Title 
     heading: 'SafeToNet Live Chat',
-    //Chat form Welcome Message
+    //Chat form Welcome Message 
     textBanner: 'Hi, welcome to SafeToNet! To start a chat with us, we just need a few details. We will then put you in contact with our next available agent!',
-    //Submit Button Label.
+    //Submit Button Label. 
     SubmitLabel: 'Start chat',
-    //Fields List - Maximum is 5
-    //All the values are mandatory and the script will not work if not available.
+    //Fields List - Maximum is 5 
+    //All the values are mandatory and the script will not work if not available. 
     fields: {
       field1: {
-        //Type can be either text or title
-        type: "title",
-        //Label for Field Title, can be in any language
-        label: "Title",
-        //Field ID for Title
-        fieldId: "title",
-        //Required "yes" or "no"
-        required: "yes",
-        //Error text to be displayed
-        error: "Please enter a valid title"
-      },
-      field2: {
-        //Type for Name - Do not Change
+        //Type for Name - Do not Change 
         type: "name",
-        //Label for Field Name, can be in any language
-        label: "Name",
-        //Default - Field ID for Name - Do Not Change
+        //Label for Field Name, can be in any language 
+        label: "Full Name",
+        //Default - Field ID for Name - Do Not Change 
         fieldId: "name",
-        //Required "yes" or "no"
+        //Required "yes" or "no" 
         required: "yes",
-        //Error text to be displayed
+        //Error text to be displayed 
         error: "Please enter a valid name"
       },
-      field3: {
-        //Type for Email - Do Not Change
+      field2: {
+        //Type for Email - Do Not Change 
         type: "email",
-        //Label for Field Email, can be in any language
+        //Label for Field Email, can be in any language 
         label: "Email",
-        //Default - Field ID for Email - Do Not Change
+        //Default - Field ID for Email - Do Not Change 
         fieldId: "email",
-        //Required "yes" or "no"
+        //Required "yes" or "no" 
         required: "yes",
-        //Error text to be displayed
+        //Error text to be displayed 
         error: "Please enter a valid email address"
       },
-      field4: {
-        //Type for Phone - Do Not Change
+      field3: {
+        //Type for Phone - Do Not Change 
         type: "phone",
-        //Label for Field Phone, can be in any language
+        //Label for Field Phone, can be in any language 
         label: "Mobile",
-        //Default - Field ID for Phone - Do Not Change
+        //Default - Field ID for Phone - Do Not Change 
         fieldId: "phone",
-        //Required "yes" or "no"
+        //Required "yes" or "no" 
         required: "yes",
-        //Error text to be displayed
+        //Error text to be displayed 
         error: "Please enter a valid mobile number"
       }
     }
@@ -275,14 +239,14 @@ if (contact_EN_Local || contact_EN_Live) {
     host: "https://wchat.freshchat.com",
     config: {
       cssNames: {
-        //The below element is mandatory. Please add any custom class or leave the default.
+        //The below element is mandatory. Please add any custom class or leave the default. 
         widget: 'custom_fc_frame',
-        //The below element is mandatory. Please add any custom class or leave the default.
+        //The below element is mandatory. Please add any custom class or leave the default. 
         expanded: 'custom_fc_expanded'
       }
     },
     onInit: function () {
-      //console.log('widget init');
+      //console.log('widget init'); 
       fcPreChatform.fcWidgetInit(preChatTemplate);
     }
   };
